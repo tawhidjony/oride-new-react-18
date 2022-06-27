d9c0956aeb8c497f6294b6157dbb4b624e3271c1
Tawhidur Rahman <tawhid@jadroo.com.bd>
2022-06-27 16:06:03 +0600

    require

---
+++ src/Route/PublicAuth.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import useAuth from "../hook/useAuth";
+
+const PublicAuth = () => {
+  const { auth, setAuth } = useAuth();
+  const authLogin = localStorage.getItem("auth");
+  const location = useLocation();
+  useEffect(() => {
+    if (authLogin) {
+      setAuth("true");
+    }
+  }, [auth, authLogin]);
+  return auth === "true" ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+};
+
+export default PublicAuth;
